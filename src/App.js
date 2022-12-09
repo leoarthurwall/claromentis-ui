a28733ae1f4e2d8d44229dfe6dc14278424f828e
leoarthurwall <leowall93@gmail.com>
2022-12-09 23:54:37 +0000

    created and positioned the outer container of the main component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
-import './index.css';
+import styled from "styled-components";
+
+const AppContainer = styled.div`
+  margin-top: 5%;
+  height: 60vh;
+  width: 80vw;
+  background-color: white;
+  border: 2px solid blue;
+`;
 
 function App() {
-  return (
-    <div className="App">
-      
-      hello world
-    </div>
-  );
+  return <AppContainer>Hello World</AppContainer>;
 }
 
 export default App;

d69f3837f5b801a623c0f984f9fa9166beea81be
leoarthurwall <leowall93@gmail.com>
2022-12-10 00:56:45 +0000

    gave flex layout to the header section and added a header text tag

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,10 +5,23 @@ const HeaderContainer = styled.header`
   height: 50px;
   width: 100%;
   border-bottom: 1px solid rgb(210, 210, 210);
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+const HeaderText = styled.div`
+  font-size: 16px;
+  font-weight: 500;
 `;
 
 const Header = () => {
-  return <HeaderContainer>Header</HeaderContainer>;
+  return (
+    <HeaderContainer>
+      <HeaderText>Badges</HeaderText>
+    </HeaderContainer>
+  );
 };
 
 export default Header;

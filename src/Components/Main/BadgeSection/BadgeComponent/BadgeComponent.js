fde5826a530bb7b348e515bed88b2bdd4628172f
leoarthurwall <leowall93@gmail.com>
2022-12-10 00:43:22 +0000

    created an upper and lower section for the badge component and a border top for the lower

--- src/Components/Main/BadgeSection/BadgeComponent/BadgeComponent.js
+++ src/Components/Main/BadgeSection/BadgeComponent/BadgeComponent.js
@@ -1,17 +1,30 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from "react";
+import styled from "styled-components";
 
 const BadgeContainer = styled.div`
-width: 100%;
-height: 100%;
-max-height: 250px;
-border: 1px solid red;
+  width: 100%;
+  height: 100%;
+  max-height: 250px;
+  border: 1px solid rgb(210, 210, 210)
+`;
 
-`
+const UpperContainer = styled.div`
+  height: 75%;
+  width: 100%;
+`;
+const LowerContainer = styled.div`
+  height: 75%;
+  width: 100%;
+  border-top: 1px solid rgb(210, 210, 210);
+
+`;
 const BadgeComponent = () => {
   return (
-    <BadgeContainer>Badge</BadgeContainer>
-  )
-}
+    <BadgeContainer>
+      <UpperContainer></UpperContainer>
+      <LowerContainer></LowerContainer>
+    </BadgeContainer>
+  );
+};
 
-export default BadgeComponent
+export default BadgeComponent;

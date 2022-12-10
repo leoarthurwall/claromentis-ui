fa0a4329adb0f76fd2f36c0fba7b4e0cdd006ff4
leoarthurwall <leowall93@gmail.com>
2022-12-10 00:08:07 +0000

    set up the basic layout for the main container. divided main into badges and profile

---
+++ src/Components/Main/Main.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MainContainer = styled.main`
+    Width: 100%;
+    height: 100%;
+    padding: 10px;
+`
+const BadgesSection = styled.section``
+
+const ProfileSection = styled.section``
+
+const Main = () => {
+  return (
+    <MainContainer>
+        <BadgesSection></BadgesSection>
+        <ProfileSection></ProfileSection>
+    </MainContainer>
+  )
+}
+
+export default Main

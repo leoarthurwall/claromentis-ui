3de392c317aca63762c8d545e4f8a569a9f1e497
leoarthurwall <leowall93@gmail.com>
2022-12-11 20:43:57 +0000

    finished the sortby component styling

--- src/Components/Main/ProfileSection/SortBy/SortBy.js
+++ src/Components/Main/ProfileSection/SortBy/SortBy.js
@@ -4,24 +4,34 @@ import styled from "styled-components";
 const SortByContainer = styled.section`
   display: flex;
   height: auto;
-  width: 1005;
+  width: 100%;
+  margin-top: 15px;
 `;
-const Form = styled.form``;
-const Label = styled.label``;
-const Select = styled.select``
-const Option = styled.option``
+const Form = styled.form`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+const Label = styled.label`
+font-size: 14px;
+color: #222529;`;
+const Select = styled.select`
+  padding: 5px;
+  border:  1px solid rgb(210, 210, 210);
+  color: #4A5056;
+`;
+const Option = styled.option``;
 
 const SortBy = () => {
   return (
     <SortByContainer>
       <Form>
         <Label for="sort">Sort by</Label>
-        <Select id="sort">
-          <Option>Alphabetical: A-Z</Option>
-          <Option>Alphabetical: Z-A</Option>
-
+        <Select id="sort" name="staff">
+          <Option value="Alphabetical: A-Z">Alphabetical: A-Z</Option>
+          <Option value="Alphabetical: Z-A">Alphabetical: Z-A</Option>
         </Select>
-
       </Form>
     </SortByContainer>
   );

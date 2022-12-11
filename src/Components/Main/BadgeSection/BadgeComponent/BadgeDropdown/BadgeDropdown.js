65e421cc5ec17722ca0eef2a4dc8bf60811f6f3a
leoarthurwall <leowall93@gmail.com>
2022-12-11 23:32:37 +0000

    dropdown has been styled, slight glitch at the moment and need to set an off click

--- src/Components/Main/BadgeSection/BadgeComponent/BadgeDropdown/BadgeDropdown.js
+++ src/Components/Main/BadgeSection/BadgeComponent/BadgeDropdown/BadgeDropdown.js
@@ -20,13 +20,34 @@ const SelectContainer = styled.div`
 `;
 
 const OptionsContainer = styled.div`
-  background: #f8f9fa;
-  height: 50px;
-  width: 50px;
+  background: #fff;
+  height: auto;
+  width: auto;
   position: absolute;
   top: 24px;
   left: 0px;
+  border: 1px solid rgb(210, 210, 210);
+  border-radius: 5px;
 `;
+
+const UL = styled.ul`
+  list-style-type: none;
+  padding: 0;
+  margin: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  
+  padding: 10px 0;
+`;
+const LI = styled.li`
+padding: 10px 50px 10px 10px;
+
+&:hover{
+    background: #e0e1e2;
+}
+`;
+
 const BadgeDropdown = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
 
@@ -37,7 +58,14 @@ const BadgeDropdown = () => {
     <>
       <SelectContainer onClick={handleDropdownClick}>
         <HiDotsVertical />
-      {isDropdownOpen ? <OptionsContainer></OptionsContainer> : null}
+        {isDropdownOpen ? (
+          <OptionsContainer>
+            <UL>
+              <LI>Edit</LI>
+              <LI>Delete</LI>
+            </UL>
+          </OptionsContainer>
+        ) : null}
       </SelectContainer>
     </>
   );

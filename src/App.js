d4f346e98966e5f130fd7a01a434cc89a9e98404
leoarthurwall <leowall93@gmail.com>
2022-12-11 00:31:27 +0000

    set the height of the app and the profile to auto, so they naturally fit the content

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Main from "./Components/Main/Main";
 
 const AppContainer = styled.div`
   margin-top: 5%;
-  height: 60vh;
+  height: auto;
   width: 80vw;
   background-color: white;
   border: 2px solid blue;
--- src/Components/Main/ProfileSection/ProfileSection.js
+++ src/Components/Main/ProfileSection/ProfileSection.js
@@ -2,7 +2,7 @@ import React from 'react'
 import styled from 'styled-components'
 
 const ProfileContainer = styled.section`
-  height: 100%;
+  height: auto;
   width: 25%;
   padding-left: 10px;
   border-left: 1px solid rgb(210, 210, 210);

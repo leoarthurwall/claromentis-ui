e74b08252abff1d7c193320237280d0a827e1931
leoarthurwall <leowall93@gmail.com>
2022-12-11 00:08:14 +0000

    dynamically added alt tags for the badge icons

--- src/Components/Main/BadgeSection/BadgeSection.js
+++ src/Components/Main/BadgeSection/BadgeSection.js
@@ -16,11 +16,11 @@ const BadgeContainer = styled.section`
 const BadgeSection = () => {
   return (
     <BadgeContainer>
-        <BadgeComponent badgeName={"Sales Superstar"} badgeSrc={"assets/badge-bg/badge-1.svg"}/>
-        <BadgeComponent badgeName={"Customer Champion"} badgeSrc={"assets/badge-bg/badge-2.svg"}/>
-        <BadgeComponent badgeName={"Creative Thinker"} badgeSrc={"assets/badge-bg/badge-3.svg"}/>
-        <BadgeComponent badgeName={"Content Crafter"} badgeSrc={"assets/badge-bg/badge-4.svg"}/>
-        <BadgeComponent badgeName={"Tech Trailblazer"} badgeSrc={"assets/badge-bg/badge-5.svg"}/>
+        <BadgeComponent badgeName={"Sales Superstar"} badgeSrc={"assets/badge-bg/badge-1.svg"} badgeAlt={"red badge icon"}/>
+        <BadgeComponent badgeName={"Customer Champion"} badgeSrc={"assets/badge-bg/badge-2.svg"} badgeAlt={"yellow badge icon"}/>
+        <BadgeComponent badgeName={"Creative Thinker"} badgeSrc={"assets/badge-bg/badge-3.svg"} badgeAlt={"blue badge icon"}/>
+        <BadgeComponent badgeName={"Content Crafter"} badgeSrc={"assets/badge-bg/badge-4.svg"} badgeAlt={"light blue badge icon"}/>
+        <BadgeComponent badgeName={"Tech Trailblazer"} badgeSrc={"assets/badge-bg/badge-5.svg"} badgeAlt={"green badge icon"}/>
     </BadgeContainer>
   );
 };

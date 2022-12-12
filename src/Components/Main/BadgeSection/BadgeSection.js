2ed9802686ad30be68bdc30927b15318fa7a71b8
leoarthurwall <leowall93@gmail.com>
2022-12-12 10:27:19 +0000

    set the grid row and column gap to align with the design better

--- src/Components/Main/BadgeSection/BadgeSection.js
+++ src/Components/Main/BadgeSection/BadgeSection.js
@@ -9,7 +9,8 @@ const BadgeContainer = styled.section`
 
   display: grid;
   grid-template-columns: 1fr 1fr 1fr;
-  grid-gap: 1vw;
+  grid-row-gap: 15px;
+  grid-column-gap: 25px;
 
   @media (max-width: 1280px) {
     grid-template-columns: 1fr 1fr;
